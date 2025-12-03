90b98dfa19dc24364d1a008c3cdca7e595507f16
Dteeech <marshall.isaac@protonmail.com>
2025-12-03 11:20:45 +0100

    feat: implemented the breadcrumb and the link to the applications list / kanban

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { AppSidebar } from "@/components/app-sidebar"
+import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb"
+import { Separator } from "@/components/ui/separator"
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/Sidebar"
 // import { useRouter } from "next/navigation"
 // import { useEffect } from "react"
@@ -11,7 +13,7 @@ export default function AdminLayout({
   children: React.ReactNode
 }) {
   // const router = useRouter()
-  
+
   // Plus tard, décommenter pour activer la protection d'authentification :
   // useEffect(() => {
   //   const isAuthenticated = checkAuth() // votre logique d'auth
@@ -26,6 +28,8 @@ export default function AdminLayout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b$ px-4">
           <SidebarTrigger className="-ml-1 text-white bg-secondary-900 hover:bg-secondary-900" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <DynamicBreadcrumb />
         </header>
         {children}
       </SidebarInset>
